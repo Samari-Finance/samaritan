c95941f6bc6238c676d24b7a2468396b8f386b72
khm <kh.mikkelsen@hotmail.com>
2021-05-15 22:35:09 +0200

    Updated commands to reflect release

--- core/commands.py
+++ core/commands.py
@@ -12,8 +12,8 @@ shilltg = '🌜 Samari is launching soon! 🌛\n' \
             '📈 CHART (V2): link\n' \
             '🔒 LIQUIDITY BURNED // OWNERSHIP RENOUNCED\n' \
             '✅ VERIFIED CONTRACT: link\n'
-shillreddit = 'Welcome Samaritans!\n'\
-    '⏳ We’re going live soon! Countdown Link \n'\
+shillreddit = 'Samari finance | Charity platform, 1 day old, 1k holders, and devs are doxxed\n' \
+              'Welcome Samaritans!\n'\
     '🏵 What is Samari?\n'\
     '$SAMA is a deflationary, community-powered charitable organization. Samari also offers a yield generating protocol' \
               ' – by holding'\
@@ -25,7 +25,7 @@ shillreddit = 'Welcome Samaritans!\n'\
     'Philanthropy is for the public good, focusing on quality of life. We – the community - wish to make a difference ' \
     'where it really counts by donating to local small charitable organizations, where bureaucracy doesn’t consume ' \
               'most resources. Whether it be for animal shelters, the global pandemic ongoing crisis, planet earth ' \
-              'or an entirely different matter'\
+              'or an entirely different matter '\
     'that could use a helping hand, we do not limit ourselves to a specific cause. The community will have their say in'\
     'this with the weighted voting system. Together the Samaritans can make a difference! '\
     '$SAMA’s charity wallet is designed as such, that each % transaction fee is immediately liquidated to BNB, ' \
@@ -35,8 +35,9 @@ shillreddit = 'Welcome Samaritans!\n'\
     '💸 Tokenomics:\n'\
     '10% Tax\n• 5% to the liquidity pool\n• 3% redistribution to existing holders\n' \
     '• 2% to the charity wallet\n• 1% to the marketing wallet'\
-              'Initial Burn: 5%\nTeam: 20%\nPancakeSwap LP: 75%\nTotal Supply: 1,xxx,000,000,000\n'\
-    '\n⚖️ Anti-Whale Tokenomics:\n'\
+              'Burn: 5%\nTeam: 20%\nPancakeSwap LP: 75%\nTotal Supply: 1,273,628,335,437\n'\
+    '\n⚖️ Anti-Whale Tokenomics:\n' \
+              'Each wallet can at max only sell 1% of total supply in a timeframe of 3 hours\n'\
     '\n🌐 Team & Community:\n'\
     'The team consists of five members, that compliment one another’s skill sets in terms of development, marketing,' \
               ' and legal entities. Three members have doxxed themselves, with personal information' \
@@ -44,10 +45,15 @@ shillreddit = 'Welcome Samaritans!\n'\
               ' The remaining two members are not dismissive of this but are currently reluctant due to work' \
               ' and legal matters. The telegram currently consists of a very active, involved community of 300 members,' \
               ' that is rapidly growing.\n'\
-    '\n💰 Wallet addresses:\nCharity Wallet: missing\nMarketing: missing\nTeam: missing\n'\
-    '\n▶️ LINKS:\n🧿 Contract Address: TBA\n🔥 LP Burned: TBA\n🔒 Ownership Renounced:\n🌍 Website: Samari.Finance\n' \
-              '📱 Telegram: t.me/SamariFinance\n🕊️ Twitter: twitter.com/SamariFinance\n🥞 PancakeSwap: TBA\n' \
-              '💩 PooCoin: TBA\n'
+    '\n💰 Wallet addresses:\n' \
+              'Charity & Marketing contract: https://bscscan.com/address/0x148fc609378af4256f2e799ebceea032fc346474\n' \
+              'Team & Burn: https://bscscan.com/token/0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be?a=0x2d045410f002a95efcee67759a92518fa3fce677\n'\
+    '\n▶️ LINKS:\n🧿 Contract Address: https://bscscan.com/address/0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be\n' \
+              '🌍 Website: Samari.Finance\n' \
+              '📱 Telegram: t.me/SamariFinance\n' \
+              '🕊️ Twitter: twitter.com/SamariFinance\n' \
+              '🥞 PancakeSwap: https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be \n' \
+              '💩 PooCoin: https://poocoin.app/tokens/0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be\n'
 shillist = '\n@uniswaptalk'\
         '\n@gemcollectors'\
         '\n@cryptoM00NShots'\
@@ -188,17 +194,24 @@ commands = {
              '/shillreddit - template for reddit shill\n'
              '/shilltelegram or /shilltg - template for shill on Telegram\n'
              '/shilltwitter - template for twitter shill\n',
-    "website": 'Samari.finance',
-    "chart": '$SAMA is currently untracked due to not being launched.',  # todo
-    "price": '$SAMA is currently untracked due to not being launched.',  # todo
-    "mc": 'Market cap of $SAMA is untracked due to not being launched',  # todo
+    "website": 'https://Samari.finance',
+    "contract:": 'Contract: https://bscscan.com/address/0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be',
+    "socials": '🌐Website: https://Samari.finance\n'
+               '🐦Twitter: https://twitter.com/SamariFinance\n'
+               '📱Telegram: https://t.me/SamariFinanec\n'
+               '🎮Discord: https://discord.gg/557bPEUB\n',
+    "chart": 'https://poocoin.app/tokens/0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be',
+    "trade": 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be',
+    "price": 'Live price has not been implemented yet, sorry',  # todo
+    "mc": 'Live market cap has not been implemented yet, sorry',  # todo
     "shillist": shillist,
     "too_fast": 'Check this [message](https://t.me/c',
     "shillin": 'Available versions of shill are available:\n'
                '/shillreddit\n'
                '/shilltelegram or /shilltg\n'
                '/shilltwitter',
-    "shillreddit": shillreddit,  # todo
+    "shillreddit": shillreddit,
     "shilltelegram": shilltg,  # todo
-    "shilltwitter": 'Implemented after website is launched',  # todo
+    "shilltwitter": 'Samari.finance, brand new charity based token with 1k holders, doxxed devs, '
+                    'influencers from Instagram and YouTube planned for TONIGHT #SamariFinance',
 }
--- core/samaritan.py
+++ core/samaritan.py
@@ -15,9 +15,11 @@ class Samaritan:
         self.setup(log_level=log_level)
         self.updater = Updater(token=self._read_api(api_key_file), use_context=True)
         self.dispatcher = self.updater.dispatcher
-        self.list_timer = datetime.now() - timedelta(minutes=10)
+        self.shillist_timer = datetime.now() - timedelta(minutes=30)
+        self.shillreddit_timer = datetime.now() - timedelta(minutes=10)
         self.add_handles()
         self.shillist_msg = None
+        self.check_commands()
 
     def start(self, update, context):
         self.send_message(update, context, text=commands['start'])
@@ -28,6 +30,15 @@ class Samaritan:
     def chart(self, update, context):
         self.send_message(update, context, commands['chart'])
 
+    def trade(self, update, context):
+        self.send_message(update, context, commands['trade'])
+
+    def contract(self, update, context):
+        self.send_message(update, context, commands['contract'])
+
+    def socials(self, update, context):
+        self.send_message(update, context, commands['socials'])
+
     def price(self, update, context):
         self.send_message(update, context, commands['price'])
 
@@ -36,9 +47,9 @@ class Samaritan:
 
     def shill_list(self, update: Update, context: CallbackContext):
         now = datetime.now()
-        if self.list_timer + timedelta(minutes=10) <= now:
+        if self.list_timer + timedelta(minutes=30) <= now:
             self.shillist_msg = self.send_message(update, context, commands['shillist'])
-            self.list_timer = now
+            self.shillist_timer = now
         else:
             self.send_message_markdown(
                 update, context, text=self._prettify_reference(update, 'too_fast', self.shillist_msg.message_id.real))
@@ -51,7 +62,14 @@ class Samaritan:
         self.send_message(update, context, commands['shillin'])
 
     def shill_reddit(self, update, context):
-        self.send_message(update, context, commands['shillreddit'])
+        now = datetime.now()
+        if self.shillreddit_timer + timedelta(minutes=10) <= now:
+            self.shillreddit_timer = self.send_message(update, context, commands['shillreddit'])
+            self.shillreddit_timer = now
+        else:
+            self.send_message_markdown(
+                update, context,
+                text=self._prettify_reference(update, 'too_fast', self.shillist_msg.message_id.real))
 
     def shill_telegram(self, update, context):
         self.send_message(update, context, commands['shilltelegram'])
@@ -72,11 +90,15 @@ class Samaritan:
 
     def add_handles(self):
         self.dispatcher.add_handler(CommandHandler('chart', self.chart))
+        self.dispatcher.add_handler(CommandHandler('trade', self.trade))
+        self.dispatcher.add_handler(CommandHandler('buy', self.trade))
         self.dispatcher.add_handler(CommandHandler('start', self.start))
         self.dispatcher.add_handler(CommandHandler('commands', self.start))
         self.dispatcher.add_handler(CommandHandler('price', self.price))
         self.dispatcher.add_handler(CommandHandler('website', self.website))
         self.dispatcher.add_handler(CommandHandler('marketcap', self.mc))
+        self.dispatcher.add_handler(CommandHandler('socials', self.socials))
+        self.dispatcher.add_handler(CommandHandler('contract', self.contract))
         self.dispatcher.add_handler(CommandHandler('shill', self.shillin))
         self.dispatcher.add_handler(CommandHandler('shillin', self.shillin))
         self.dispatcher.add_handler(CommandHandler('shillreddit', self.shill_reddit))
@@ -101,3 +123,8 @@ class Samaritan:
             api_key_file = os.path.dirname(os.getcwd()) + '/' + api_key_file
         key_file = open(api_key_file)
         return key_file.read()
+
+    def check_commands(self):
+        for key in self.dispatcher.handlers.keys():
+            if key not in commands:
+                print(f'Command missing: {key}')
