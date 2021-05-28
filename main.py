9214b90580b8497fd3aea5e45d020e0a42abecdc
kristian <kh.mikkelsen@hotmail.com>
2021-05-28 18:20:27 +0200

    set main api key.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from core import Samaritan
 
 if __name__ == '__main__':
     samaritan = Samaritan(log_level=logging.INFO,
-                          api_key_file='test_api_key',
+                          api_key_file='api_key',
                           db_path='mongo_api')
     samaritan.start_polling()
     samaritan.updater.idle()
