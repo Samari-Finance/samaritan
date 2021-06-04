f17e6dd96044b2fcd7971d2019a7e7469f154018
khm <kh.mikkelsen@hotmail.com>
2021-06-04 13:26:46 +0200

    Improved usability and flexibility of send_message

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,8 @@
 import json
 import os
+from typing import List, Union, Optional
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, ReplyMarkup
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import DEFAULT_NONE
 
@@ -22,14 +23,29 @@ def pp_json(msg):
 
 
 def send_message(update: Update, context: CallbackContext, text: str, parse_mode=DEFAULT_NONE, reply=True,
-                 disable_web_page_preview=DEFAULT_NONE, disable_notification=False):
+                 disable_web_page_preview=DEFAULT_NONE, disable_notification=False, reply_markup=None):
     reply_to_msg_id = None
     if reply:
-        reply_to_msg_id = update.message.message_id
+        if update.message is not None:
+            reply_to_msg_id = update.message.message_id
 
-    return context.bot.send_message(chat_id=update.message.chat_id,
+    return context.bot.send_message(chat_id=update.effective_chat.id,
                                     text=text,
                                     parse_mode=parse_mode,
                                     reply_to_message_id=reply_to_msg_id,
                                     disable_web_page_preview=disable_web_page_preview,
-                                    disable_notification=disable_notification)
+                                    disable_notification=disable_notification,
+                                    reply_markup=reply_markup)
+
+def build_menu(
+    buttons: List[InlineKeyboardButton],
+    n_cols: int,
+    header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None,
+    footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None
+) -> List[List[InlineKeyboardButton]]:
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
+    if footer_buttons:
+        menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
+    return menu
