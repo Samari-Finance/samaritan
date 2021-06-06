9e603217df3da6925e54ba626a52af458553ac05
khm <kh.mikkelsen@hotmail.com>
2021-06-06 18:33:30 +0200

    Documentation.

--- core/utils.py
+++ core/utils.py
@@ -1,29 +1,25 @@
 import json
 import os
-from typing import List, Union, Optional
+from io import BytesIO
+from typing import List, Union
 
-from telegram import Update, InlineKeyboardButton, ReplyMarkup
+from telegram import Update, InlineKeyboardButton, Message
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import DEFAULT_NONE
 
-MARKDOWN_V2 = 'MarkdownV2'
-HTML = 'html'
+
+def send_message(update: Update, context: CallbackContext,
+                 text: str,
+                 parse_mode=DEFAULT_NONE,
+                 reply=True,
+                 disable_web_page_preview=DEFAULT_NONE,
+                 disable_notification=False,
+                 reply_markup=None) -> Message:
+    """A wrapper around update.send_message
 
 
-def read_api(api_key_file):
-    if not os.path.exists(api_key_file):
-        api_key_file = os.path.dirname(os.getcwd()) + '/' + api_key_file
-    key_file = open(api_key_file)
-    return key_file.read()
-
-
-def pp_json(msg):
-    json_str = json.loads(msg)
-    print(json.dumps(json_str, indent=3))
-
-
-def send_message(update: Update, context: CallbackContext, text: str, parse_mode=DEFAULT_NONE, reply=True,
-                 disable_web_page_preview=DEFAULT_NONE, disable_notification=False, reply_markup=None):
+    :return: The outgoing telegram.Message instance.
+    """
     reply_to_msg_id = None
     if reply:
         if update.message is not None:
