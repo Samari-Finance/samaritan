b98dabe5393d01d5c712f448f8a70a4a93e6d96f
khm <kh.mikkelsen@hotmail.com>
2021-06-17 04:15:22 +0200

    Fixed price/mc fetching and restructured so graphcli.py's role is simply to present samaritan.py with the result, not intermediate results.

--- core/bitquery/graphcli.py
+++ core/bitquery/graphcli.py
@@ -1,20 +1,24 @@
 from pprint import pprint
+from statistics import mean
 
 from core.bitquery import run_query
+from core.samaritable import Samaritable
+from core.utils.utils import log_entexit
 
 
-class GraphQLClient:
+class GraphQLClient(Samaritable):
 
-    def __init__(self,
-                 api_key_path: str = None):
+    def __init__(self):
+        super().__init__()
         self.sama_addr = '0xb255cddf7fbaf1cbcc57d16fe2eaffffdbf5a8be'
 
+    @log_entexit
     def q_price(self) -> dict:
         query = """
             query{
   ethereum(network: bsc) {
     dexTrades(
-      options: {limit: 1, desc: "block.height"}
+      options: {limit: 10, desc: "block.height"}
       exchangeName: {is: "Pancake v2"}
       baseCurrency: {is: "%s"}
     ) {
@@ -61,5 +65,35 @@ class GraphQLClient:
 }
         """ % self.sama_addr
         return run_query(query)
+
+    @log_entexit
+    def fetch_price(self):
+        prices = []
+        response = self.q_price()
+        for trade in self._dex_trades(response):
+            if self.is_buy(trade):
+                buy_amount = trade['buyAmount']
+                sell_amount_usd = trade['sellAmountInUsd']
+                prices.append(sell_amount_usd / buy_amount)
+            else:
+                buy_amount_usd = trade['buyAmountInUsd']
+                sell_amount = trade['sellAmount']
+                prices.append(buy_amount_usd / sell_amount)
+
+        print(prices)
+
+        return mean(prices)
+
+    @log_entexit
+    def fetch_mc(self):
+        return self.fetch_price() * 1273628335437
+
+    @log_entexit
+    def is_buy(self, trade):
+        return trade['buyCurrency']['symbol'] == 'SAMA'
+
+    @log_entexit
+    def _dex_trades(self, path: dict):
+        return path['data']['ethereum']['dexTrades']
 
 
