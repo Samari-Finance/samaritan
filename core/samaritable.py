8b9a8c6bf07d189331d7b63883e9f2b0cbd1fab3
khm <kh.mikkelsen@hotmail.com>
2021-06-17 04:16:02 +0200

    Refactoring method names.

--- core/samaritable.py
+++ core/samaritable.py
@@ -3,12 +3,12 @@ import logging
 
 class Samaritable:
     def __init__(self):
-        self.log = self._aggregate_log_name()
+        self.log = self._aggregate_logger()
 
     def __set_name__(self, owner, name):
         self.name = name
 
-    def _aggregate_log_name(self):
+    def _aggregate_logger(self):
         if self.__class__.__name__.lower() == 'samaritable' or self.__class__.__name__.lower() == 'samaritan':
             name = 'samaritan'
         else:
