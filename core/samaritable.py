d1d37cf7a3984a23d65d5f5d53ca84adf3d50cb3
khm <kh.mikkelsen@hotmail.com>
2021-06-19 03:38:27 +0200

    Moved admin wrappers into utils_mod.py

--- core/samaritable.py
+++ core/samaritable.py
@@ -1,12 +1,6 @@
 import logging
 from abc import ABC, abstractmethod
-from functools import wraps
-
-from telegram import ChatMember
-
-from core import CREATOR, ADMIN
 from core.db import MongoConn
-from core.utils.utils_mod import is_superadmin
 
 
 class Samaritable(ABC):
@@ -24,35 +18,6 @@ class Samaritable(ABC):
         else:
             name = 'samaritan.'+self.__class__.__name__.lower()
         return logging.getLogger(name)
-
-    def only_admins(self, method):
-        @wraps(method)
-        def inner(self_inner, *args, **kwargs):
-            user_id = args[0].effective_user.id  # args[0]: update
-            chat_id = args[0].effective_chat.id
-            is_admin = args[1].bot.get_chat_member(chat_id, user_id).status == ADMIN
-            if not is_admin:
-                self.log.debug(f'Unauthorized access denied on %s for %s : %s.',
-                               method.__name__, user_id, args[0].message.chat.username)
-                args[0].message.reply_text('You do not have the required permissions to access this command')
-                return None  # quit handling command
-            return method(self_inner, *args, **kwargs)
-        return inner
-
-    def only_superadmin(self, method):
-        @wraps(method)
-        def inner(self_inner, *args, **kwargs):
-            # args[0]: update
-            user_id = args[0].effective_user.id  # args[0]: update
-            chat_id = args[0].effective_chat.id
-            user = args[1].bot.get_chat_member(chat_id, user_id)
-            if not is_superadmin(user):
-                self.log.debug(f'Unauthorized access denied on %s for %s : %s.',
-                               method.__name__, user_id, args[0].message.chat.username)
-                args[0].message.reply_text('You do not have the required permissions to access this command')
-                return None  # quit handling command
-            return method(self_inner, *args, **kwargs)
-        return inner
 
     @abstractmethod
     def add_handlers(self, dp):
--- core/utils/utils_mod.py
+++ core/utils/utils_mod.py
@@ -1,6 +1,39 @@
+from functools import wraps
+
 from telegram import ChatMember
 
 from core import ADMIN
+
+
+def only_admins(method):
+    @wraps(method)
+    def inner(self, *args, **kwargs):
+        user_id = args[0].effective_user.id  # args[0]: update
+        chat_id = args[0].effective_chat.id
+        is_admin = args[1].bot.get_chat_member(chat_id, user_id).status == ADMIN
+        if not is_admin:
+            self.log.debug(f'Unauthorized access denied on %s for %s : %s.',
+                           method.__name__, user_id, args[0].message.chat.username)
+            args[0].message.reply_text('You do not have the required permissions to access this command')
+            return None  # quit handling command
+        return method(self, *args, **kwargs)
+    return inner
+
+
+def only_superadmin(method):
+    @wraps(method)
+    def inner(self, *args, **kwargs):
+        # args[0]: update
+        user_id = args[0].effective_user.id  # args[0]: update
+        chat_id = args[0].effective_chat.id
+        user = args[1].bot.get_chat_member(chat_id, user_id)
+        if not is_superadmin(user):
+            self.log.debug(f'Unauthorized access denied on %s for %s : %s.',
+                           method.__name__, user_id, args[0].message.chat.username)
+            args[0].message.reply_text('You do not have the required permissions to access this command')
+            return None  # quit handling command
+        return method(self, *args, **kwargs)
+    return inner
 
 
 def is_superadmin(user: ChatMember):
